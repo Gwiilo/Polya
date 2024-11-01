86cb549d3c645a6f5d1dc4418bcb50cbf52ed56e
Gwiilo <daenylb@gmail.com>
2024-11-01 20:01:45 +0000

    Update VSCode tasks and launch configuration; enhance build script with Vulkan SDK settings

--- build.bat
+++ build.bat
@@ -2,9 +2,9 @@
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-SET includes=
-SET links=
-SET defines=
+SET includes=/Isrc /I%VULKAN_SDK%/Include
+SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib
+SET defines=/D DEBUG
 
 echo "Building main..."
 
